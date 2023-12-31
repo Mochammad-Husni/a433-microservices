5b21114b29e37f252da9cad8c9a822fa1ae1ab81
mochu2010@gmail.com <mochu2010@gmail.com>
2023-12-31 07:23:17 +0700

    Add script and CI config

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#build Docker image dari berkas Dockerfile
+docker build -t ghcr.io/mochammad-husni/karsajobs:latest .
+
+#login ke GitHub Package
+echo $CR_PAT | docker login ghcr.io -u mochammad-husni --password-stdin
+
+#push image ke GitHub Package
+docker push ghcr.io/mochammad-husni/karsajobs:latest
